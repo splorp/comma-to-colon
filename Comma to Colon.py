d241e847c24241d3819acd9fba9785a14eb1b226
Grant Hutchinson <grant@splorp.com>
2024-09-09 14:49:19 -0600

    Comment formatting, edits for clarity

--- Comma to Colon.py
+++ Comma to Colon.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+#
 # Comma to Colon
-# A script to turn a CSV file into a folder of Kirby CMS content files.
-# Try Kirby for free: https://getkirby.com/
-
+#
+# Python 3 script that converts a CSV file into
+# a folder of content files for use with Kirby CMS
+# https://getkirby.com/
+#
+# Based on the original script by Myles Winstone
+# https://github.com/myleswrite/Comma-to-Colon
 
 def filesafe(filename): # Create a Kirby safe filename
     safename = filename.replace(' ','-')
@@ -35,20 +40,25 @@ directory = input('What is the name of the directory to save the files? ')
 sep = '\n\n----\n\n'
 with open(csvsource, 'r') as f: # Open the file
     worklist = csv.DictReader(f)
-    for row in worklist: # Turn each row into a Kirby content txt file
+    # Convert each row in the CSV into a Kirby content file
+    for row in worklist:
         line = ''
         try:
-            title = row['title'] # title is the one heading that must be present
-            filename = filesafe(title) + '/' + blueprint # Create a Kirby safe folder and use blueprint name
+            # A heading named 'title' must be present
+            title = row['title']
+            # Create a safely named folder and use the named Blueprint
+            filename = filesafe(title) + '/' + blueprint
             print('Creating ' + filesafe(title) + '.txt')
         except:
-            print("No 'title' item found so stopping. 'title' must be a heading and written in lowercase for the conversion to work.")
+            print("No 'title' heading found. A column heading named 'title' is required and must be lowercase.")
             exit()
-        for val,item in row.items(): # Go through each column of each row in the CSV to create a Kirby file.
+        # Step through each column of each row in the CSV to create content files
+        for val,item in row.items():
             line += val + ': ' + item + sep
         fullfile = directory + '/' + filename
         os.makedirs(os.path.dirname(fullfile), exist_ok=True)
-        with open(fullfile,'w+') as sf: # Write the content txt file
+        # Save the content file
+        with open(fullfile,'w+') as sf:
             sf.write(line)
 print("Finished")
 exit()
